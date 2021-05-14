ec4f42aca8c08af80ee58fd16d2b3e3c169e4b92
monk-wellington <mwellington@reflexion.com>
2021-05-13 18:49:31 -0700

    Wire through ffmpeg library and display video duration on page

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video-submit', (_, path) => {
+    ffmpeg.ffprobe(path, (err, { format: { duration } }) => {
+        console.log('File duration is:', duration);
+        console.log('Ya heard!');
+        mainWindow.webContents.send('video-duration', duration);
+    });
 });
